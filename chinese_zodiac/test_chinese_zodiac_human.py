58cebd8c5990133420f248688fe7d34af8fbc997
Rajkjain03 <rjainc333@gmail.com>
2025-11-11 22:32:55 +0530

    updated human property-based tests

--- chinese_zodiac/test_chinese_zodiac_human.py
+++ chinese_zodiac/test_chinese_zodiac_human.py
@@ -5,3 +5,10 @@ from chinese_zodiac import chinese_zodiac
 def test_valid_sign(year):
     valid = {'Dragon','Snake','Horse','Sheep','Monkey','Rooster','Dog','Pig','Rat','Ox','Tiger','Hare'}
     assert chinese_zodiac(year) in valid
+
+@given(st.integers(min_value=1900, max_value=2100))
+def test_cycle_length(year):
+    sign_current = chinese_zodiac(year)
+    sign_next_cycle = chinese_zodiac(year + 12)
+    assert sign_current == sign_next_cycle
+
